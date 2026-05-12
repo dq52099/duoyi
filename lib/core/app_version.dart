910c6eda378c8f6aec7faafca60738523c0d6ad5
dq52099 <1851939263@qq.com>
2026-05-12 06:13:43 +0000

    fix: raise android version code

--- lib/core/app_version.dart
+++ lib/core/app_version.dart
@@ -1,5 +1,5 @@
 class AppVersion {
-  static const name = '1.0.13';
-  static const build = 14;
+  static const name = '1.0.14';
+  static const build = 100014;
   static const display = 'v$name';
 }
