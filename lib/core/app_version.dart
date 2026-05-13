70551581b9a16b148d0a02a942b78f86516082c1
dq52099 <1851939263@qq.com>
2026-05-13 06:01:04 +0000

    chore: bump version to 1.0.21

--- lib/core/app_version.dart
+++ lib/core/app_version.dart
@@ -1,5 +1,5 @@
 class AppVersion {
-  static const name = '1.0.20';
-  static const build = 100020;
+  static const name = '1.0.21';
+  static const build = 100021;
   static const display = 'v$name';
 }
