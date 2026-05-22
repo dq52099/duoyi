eebefd877cb92e38f517b7727968990a5cc64227
dq52099 <1851939263@qq.com>
2026-05-22 16:33:13 +0000

    Release v1.1.6

--- lib/core/app_version.dart
+++ lib/core/app_version.dart
@@ -1,5 +1,5 @@
 class AppVersion {
-  static const name = '1.1.5';
-  static const build = 110005;
+  static const name = '1.1.6';
+  static const build = 110006;
   static const display = 'v$name';
 }
