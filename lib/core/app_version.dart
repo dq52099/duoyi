77ca9f64d1f2861300b8835c0719a6715fafb3a0
dq52099 <1851939263@qq.com>
2026-06-04 21:05:30 +0000

    Sync app version constants

--- lib/core/app_version.dart
+++ lib/core/app_version.dart
@@ -1,5 +1,5 @@
 class AppVersion {
-  static const name = '1.1.28';
-  static const build = 130100;
+  static const name = '1.1.29';
+  static const build = 130101;
   static const display = 'v$name';
 }
